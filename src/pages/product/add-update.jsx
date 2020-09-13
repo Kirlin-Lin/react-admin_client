52bf9485cafef92c7af4de057f516cef2b07b22b
fishshit <war_chariot@163.com>
2020-09-13 18:31:44 +0800

    1、商品列表页面完成

---
+++ src/pages/product/add-update.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+
+
+/*
+Product的详情子路由组件
+ */
+export default class ProductAddUpdate extends Component {
+
+  render(){
+    return(
+      <div>ProductAddUpdate</div>
+    )
+  }
+}
